2332c77d25da638f07dea47c47281a4a05a8e473
JonJaryongLee <director9303@gmail.com>
2023-02-01 11:16:49 +0900

    230201

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style> 
+#app {
+  font-family: 'Noto Sans KR', sans-serif;
+}
+
+ul {
+  padding-left: 0;
+  list-style: none;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>home</h1>
+    <ul>
+      <li>밥먹기</li>
+      <li>영화보기</li>
+      <li>차마시기</li>
+      <button type="button" class="btn btn-primary" v-on:click="test">
+        Primary
+      </button>
+      <span class="material-symbols-outlined"> expand_more </span>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    async test() {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        if (response.data) {
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
